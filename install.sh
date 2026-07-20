008b443d7929a51348b94cedd7235fcc4add364e
sanbob <saintsanbob@gmail.com>
2026-07-20 17:28:41 +0800

    feat: add Grafana datasource configuration and update install script for Grafana setup

--- install.sh
+++ install.sh
@@ -10,10 +10,11 @@ fi
 
 mkdir -p "${APP_DIR}"
 mkdir -p "${APP_DIR}/prometheus"
+mkdir -p "${APP_DIR}/grafana"
 
 cp ./data/compose.yaml "${APP_DIR}"
 envsubst < ./data/prometheus.template.yaml > "${APP_DIR}/prometheus/prometheus.yaml"
+cp ./data/grafana/* "${APP_DIR}/grafana/"
 
 cd "${APP_DIR}"
-
 docker compose up -d
