f29ba207901e2dc03536d728507b16dc25fb69f9
sanbob <saintsanbob@gmail.com>
2026-07-20 14:00:11 +0800

    feat: add removal script

---
+++ remove.sh
@@ -0,0 +1,15 @@
+
+if [ -f .env ]; then
+    while IFS= read -r line || [ -n "$line" ]; do
+        case "$line" in
+            ""|"#"*) continue ;;
+            *) export "$line" ;;
+        esac
+    done < .env
+fi
+
+cd "${APP_DIR}"
+
+docker compose down -v
+
+rm -rf "${APP_DIR}"
