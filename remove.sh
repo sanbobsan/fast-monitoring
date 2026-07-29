f5dd33c60833812688dcb02d6608bad430f5b4b6
sanbob <saintsanbob@gmail.com>
2026-07-29 13:13:31 +0800

    feat: add default env vars to install.sh and remove.sh

--- remove.sh
+++ remove.sh
@@ -1,4 +1,10 @@
 
+APP_DIR="${APP_DIR:-/opt/fast-monitoring}"
+GRAFANA_PORT="${GRAFANA_PORT:-3000}"
+GRAFANA_USER="${GRAFANA_USER:-user}"
+GRAFANA_PASSWORD="${GRAFANA_PASSWORD:-password}"
+NODE_PORT="${NODE_PORT:-9100}"
+
 if [ -f .env ]; then
     while IFS= read -r line || [ -n "$line" ]; do
         case "$line" in
